7799fb710e8487e3de249223db28eba75c49e21a
Chad Hinds <chadrickj8@gmail.com>
2020-07-28 10:24:42 -0400

    Add info from backend to component state

--- src/Components/YupPage.js
+++ src/Components/YupPage.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const YupPage = () => {
-    return (
-        <div>
-           Yup! 
-        </div>
-    );
-};
+class YupPage extends Component {
+    state = {
+        
+    }
+    async componentDidMount() {
+       try {
+           let info = await axios.get('https://api.yup.io/posts/post/12294')
+           console.log(info.data)
+
+        //Put the values requested from the backend into component state
+        this.setState({
+            thumbnail: info.data.previewData.img,
+            popularity: info.data.weights.popularity,
+            intelligence: info.data.weights.intelligence,
+            funny: info.data.weights.funny
+        })
+       }
+       catch(err) {
+           console.log(err)
+       }
+    }
+
+    render() {
+        return (
+            <div>
+                <img src={this.state.thumbnail} alt='thumbnail for post'/>
+                <p>{this.state.popularity}</p>
+                <p>{this.state.intelligence}</p>
+                <p>{this.state.funny}</p>
+            </div>
+        );
+    }
+}
 
 export default YupPage;
+
+// info.data.previewData.img
+// info.data.weights.popularity
+// info.data.weights.funny
+// info.data.weights.intelligence
