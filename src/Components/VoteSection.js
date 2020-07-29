e9c1b9f1837474b97a26842a21bf30839e7631bc
Chad Hinds <chadrickj8@gmail.com>
2020-07-28 22:34:22 -0400

    Insert all needed icons

--- src/Components/VoteSection.js
+++ src/Components/VoteSection.js
@@ -1,45 +1,53 @@
 import React, {useState} from 'react';
 import Tooltip from '@material-ui/core/Tooltip';
 import SvgIcon from '@material-ui/core/SvgIcon';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+
 import { ReactComponent as Funny } from '../assets/laughing.svg';
 import { ReactComponent as Heart } from '../assets/heart.svg';
 import { ReactComponent as Idea } from '../assets/idea.svg';
 import styled from "styled-components";
 
 const VoteSection = (props) => {
-    // Styled component will handle what color the underline should be
-    const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
+    // The template in the styled component will handle what color the underline should be
+    const FunnyCount = styled.p`
+        text-decoration: underline;
+    color: ${props.displayColor(props.intelSextile)}
 `;
 
-// Styled Popularity Number
+// Styled Comp for Popularity Number
 const Popularity = styled.p `
     text-decoration: underline;
     color: ${props.displayColor(props.popSextile)}
 `
-// Styled Intelligence Number
+//Styled comp for Intelligence Number
 
-// Styled Funny Number
-    console.log(props.popularity)
+// Styled comp for  Funny Number
 
     return (
         <div>
-            <Title>Members</Title>
+            <ArrowUpwardIcon />
             <Tooltip title="popularity">
+            <ArrowDownwardIcon />    
                 <SvgIcon>
                     <Heart />
                 </SvgIcon>
             </Tooltip>
-            <Popularity><span>{props.popularity}</span></Popularity>
+            {props.popSextile !== 'none'? 
+                <Popularity><span>{props.popularity}</span></Popularity> :
+                <p>{props.popularity}</p>
+            }
 
             <Tooltip title="intelligence">
                 <SvgIcon >
                     <Idea/>
                 </SvgIcon>
             </Tooltip>
-            <p><span>{props.intelligence}</span></p>
+            {props.intelSextile !== 'none' ?
+                <Popularity><span>{props.intelligence}</span></Popularity> :
+                <p>{props.intelligence}</p>
+            }
 
             <Tooltip title="funny">
                 <SvgIcon>
